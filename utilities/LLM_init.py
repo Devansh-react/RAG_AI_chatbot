055c8085cbf2cd33e170b22bb4f8dda82166394b
Devnsh Sharma <139527904+Devansh-react@users.noreply.github.com>
2026-01-07 17:06:12 +0530

    Updated LLm code

--- utilities/LLM_init.py
+++ utilities/LLM_init.py
@@ -1,8 +1,12 @@
 from dotenv import load_dotenv
 import os
-from langchain.chat_models import init_chat_model
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
-api_key = os.getenv("GOOGLE_GENAI_API_KEY")
-llm = init_chat_model("google_genai:gemini-2.0-flash", temperature=0.7, model_kwargs={"streaming": True})
+api_key = os.getenv("GOOGLE_API_KEY")
 
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0.3,
+    google_api_key=api_key
+)
